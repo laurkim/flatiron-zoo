191f89ad8d506130715f92fdb8e6ac40beb3f959
laurkim <laurakeem@gmail.com>
2018-02-05 18:03:24 -0500

    create framework and readme for assignment

---
+++ run.rb
@@ -0,0 +1,12 @@
+require_relative './Animal.rb'
+require_relative './Environment.rb'
+require_relative './Species.rb'
+require_relative './Zookeeper.rb'
+require_relative './ZookeeperEnvironment.rb'
+require 'pry'
+
+# you can paste examples of code from the README here
+# code here
+
+binding.pry
+"string of text"
